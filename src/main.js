243534e56fb55f6a03ee88ad0c33b5031dd934b2
Yoav Badt <yoavbadt@gmail.com>
2024-11-18 17:41:11 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 import Button from './components/button.vue'
 import Knob from './components/knob.vue'
 import Title from './components/title.vue'
-import Slider from './components/slider.vue'
 import OnOff from './components/onOff.vue'
 
 import { createApp } from 'vue'
@@ -12,7 +11,6 @@ export const app = createApp(App)
 app.component('Button',Button)
 app.component('Knob',Knob)
 app.component('Title',Title)
-app.component('Slider',Slider)
 app.component('OnOff',OnOff)
 app.mount('#app')
 
